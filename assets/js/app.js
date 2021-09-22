f075afe0c3fb173de59ba3e26f97699aa2b7156b
Humberto García <humbertodev14@gmail.com>
2021-09-22 17:43:34 -0400

    Completed

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,76 +1,171 @@
-const API_KEY = 'api_key=1cf50e6248dc270629e802686245c2c8';
+const d = document;
+const API_KEY = '?api_key=1cf50e6248dc270629e802686245c2c8';
 const BASE_URL = 'https://api.themoviedb.org/3';
-const API_URL = BASE_URL + '/discover/movie?sort_by=popularity.desc&' + API_KEY;
 const IMG_URL = 'https://image.tmdb.org/t/p/w500';
-const searchURL = BASE_URL + '/search/movie?' + API_KEY;
-const container = document.querySelector('.container-movies');
-const searchInput = document.getElementById('search');
-const fragment = document.createDocumentFragment();
+const POPULARITY_URL = '/discover/movie';
+const searchURL =  '/search/movie' ;
+const upComing =  '/movie/upcoming';
+const genres = '/genre/movie/list';
 
-document.addEventListener('DOMContentLoaded', (e) => {
-    fetch(API_URL)
-        .then((res) => res.json())
-        .then((data) => {
-            showMovies(data.results);
-            console.log(data.results)
+// Select elements DOM
+const container = d.querySelector('.container-movies');
+const searchInput = d.getElementById('search');
+const searchInputValue = d.getElementById('search').value;
+const fragment = d.createDocumentFragment();
+const tagsMovies = d.querySelector('.tags');
+const iconMenu = document.querySelector('.fa-bars');
+let activeMenu = false;
 
-        })
-})
 
-searchInput.addEventListener('keypress', (e) => {
-    // e.preventDefault();
-    const inputValue = searchInput.value;
-    if (e.key === 'Enter') {
-        searchMovie(inputValue);
+// DOM LOADED
+d.addEventListener('DOMContentLoaded', () => {
+    try {
+        
+        takeInfoTags(BASE_URL,genres,API_KEY);
+        takeInfo(BASE_URL,POPULARITY_URL,API_KEY);
+        menuHamburguer(tagsMovies,iconMenu);
+        takeInfoSearch(BASE_URL,searchURL,API_KEY,searchInput);
+        
+
+        
+        
+    } catch (err) {
+        console.error(`Puede ser el internet o ${err}`);
     }
 })
 
-function searchMovie(dataMovie) {
-    const currentUrl = searchURL + '&query=' + dataMovie;
-    fetch(currentUrl)
+// Functions
+// TAKE INFO
+function takeInfo(base,kind,key) {
+    try {
+        
+        const CURRENT_URL = base+kind+key;
+        fetch(CURRENT_URL)
         .then((res) => res.json())
         .then((data) => {
-            const movies = data.results;
-            showMovies(movies);
+            const results = data.results;
+            showMovies(results);
+            
+        });
+    } catch (err) {
+        console.error(`Puede ser el internet o ${err}`);
+    }
+}
 
+// TAKE INFO SEARCH
+function takeInfoSearch(base,kind,key,query) {
 
+    searchInput.addEventListener('keyup', (e) => {
+        const currentQuery = query.value;
+        
+        if (e.key === 'Enter') {
+            
+            const CURRENT_URL = base+kind+key+'&query='+currentQuery;
+            fetch(CURRENT_URL)
+            .then((res) => res.json())
+            .then((data) => {
+                const results = data.results;
+                showMovies(results);
+                console.log(results)
+            });
+        }
+    })
+        
+}
 
-        })
-};
+// TAKE INFO TAGS
+function takeInfoTags(base,kind,key) {
 
+  
+    const CURRENT_URL = base+kind+key;
+    fetch(CURRENT_URL)
+    .then((res) => res.json())
+    .then((data) => {
+        const results = data.genres;
+        showTags(results);
+        
+    });
 
+   
+
+    d.addEventListener('click', (e) => {
+        
+        
+        // console.log(e.target)
+        if (e.target.matches('.tags .tag h3')) {
+
+            const target = e.target.textContent;
+            const CURRENT_URL = BASE_URL+searchURL+API_KEY+'&query='+target;
+            fetch(CURRENT_URL)
+            .then((res) => res.json())
+            .then((data) => {
+                const results = data.results;
+                showMovies(results);
+                // console.log(results)
+            });
+            activeMenu = false;
+            tagsMovies.classList.remove('activeTags');
+        }
+    
+    })
+}
+
+// SHOW MOVIES
 function showMovies(data) {
 
     container.innerHTML = '';
-    data.forEach(element => {
-        const div = document.createElement('div');
+    data.forEach(el => {
+        const {title,poster_path,id,original_language,overview,release_date,vote_average} = el;
+        const div = d.createElement('div');
         div.classList.add('movie');
-        div.setAttribute('id',element.id);
-        const {
-            title,
-            poster_path,
-            release_date,
-            vote_average
-        } = element;
-        if (element.poster_path) {
+        div.innerHTML=`
+        <div class="containerTags">
+        <h3 class="tag">${title}</h3>
+        <img src="${getImg(poster_path)}" class="movieImg" id="${id}" alt="movieImg">
+        </div>
+        <div class="movie-content">
+        <p class="language">Language: ${original_language}</p>
+        <p class="overview">${overview}</p>
+        <p class="release">Release: ${release_date}</p>
+        <p class="vote">Vote: ${vote_average} ⭐</p>
+        </div>
+        `;
 
-
-            div.innerHTML = `
-            
-            <div class="movieImg">
-            <img src="${IMG_URL+poster_path}" alt="peliculaImg">
-            </div>
-            <div class="text-movie">
-            <h1 class="titleMovie">${title}</h1>
-            <h3 class="titleMovie">Release Date: ${release_date}</h3>
-            <span class="voteMovie">Vote: ${vote_average}⭐</span>
-            </div>
-            `;
-            fragment.append(div);
-            container.append(fragment);
-
-        }
+        fragment.append(div);
+        container.append(fragment);
     });
-
-
 }
+
+// GET IMG URL
+function getImg(dataImg) {
+    return IMG_URL+dataImg;
+} 
+
+// MENU BURGUER
+function menuHamburguer(menu,btnMenu) {
+    
+    
+    btnMenu.addEventListener('click', (e) => {
+            activeMenu = true;
+            menu.classList.toggle('activeTags');
+            btnMenu.classList.toggle('clickBtn');
+
+    });
+}
+
+// SHOW TAGS
+function showTags(data) {
+
+    data.forEach(el => {
+        const {id,name} = el;
+        const h3 = d.createElement('h3');
+        const div = d.createElement('div');
+        div.classList.add('tag');
+        div.setAttribute('id',id);
+        h3.textContent = name;
+        div.append(h3)
+        fragment.append(div);
+        tagsMovies.append(fragment);
+    });
+}
+
